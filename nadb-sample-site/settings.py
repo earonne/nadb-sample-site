c08b7895ce730c4212a8f8069e428bb24790ac87
Daniel Aronne <earonne@gmail.com>
2012-03-17 00:59:46 -0400

    part 13 completed...support for comments

--- nadb-sample-site/settings.py
+++ nadb-sample-site/settings.py
@@ -118,6 +118,7 @@ INSTALLED_APPS = (
     'nadb',
     'django.contrib.markup',
     'django.contrib.admin',
+    'django.contrib.comments',
 )
 
 # A sample logging configuration. The only tangible logging
--- nadb-sample-site/urls.py
+++ nadb-sample-site/urls.py
@@ -1,11 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^blog/', include('nadb.urls')),
-
+    url(r'^comments/', include('django.contrib.comments.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
